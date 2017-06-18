cce67cc66fa773626f18e4f7e60efd011bc8c371
DonChrapek <daniel.chrapczynski@gmail.com>
2017-06-18 18:53:50 +0200

    Initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import 'normalize.css/normalize.css';
+import 'styles/App.css';
+
+import React from 'react';
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <h1>App!</h1>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
