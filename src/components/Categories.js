e8b6a9c6ef974e566af4e743ca15a2d95a43573a
DonChrapek <daniel.chrapczynski@gmail.com>
2017-06-18 20:22:17 +0200

    Made side menu and listed categories

---
+++ src/components/Categories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Categories = (props) => {   
+    return (
+      <div>
+        { props.categories.map((category, index) => {
+          return (<h3 key={index}>{category}</h3>)
+        }) }
+      </div>
+    );
+}
+
+export default Categories;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,58 @@
-import 'normalize.css/normalize.css';
 import 'styles/App.css';
+import 'styles/reset.css';
 
 import React from 'react';
 
+/* Components */
+import Categories from './Categories';
+
 class AppComponent extends React.Component {
+    
+  stations = [
+      {
+          id: 1,
+          name: 'Lipsum Rock',
+          category: 'Rock',
+          img: ''
+      },
+      {
+          id: 2,
+          name: '80s Classics',
+          category: 'Recommended',
+          img: ''
+      },
+      {
+          id: 3,
+          name: 'House',
+          genre: 'Party',
+          img: ''
+      },
+      {
+          id: 4,
+          name: 'Trance',
+          genre: 'Party',
+          img: ''
+      },
+      {
+          id: 5,
+          name: 'Classical music',
+          genre: 'Others',
+          img: ''
+      }
+  ]
+  
+  categories = ['Recommended', 'Rock', 'Pop', 'Party', 'Rap', 'Others']
+     
   render() {
     return (
-      <h1>App!</h1>
+      <div id='app-content'>
+        <div id='side-panel'>
+            <Categories categories={this.categories}/>
+        </div>
+        <div className='main-section'>
+          <h1>App!</h1>
+        </div>
+      </div>
     );
   }
 }
